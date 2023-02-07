46f79545dec12224aaf2d4688f611b172c9cfebe
Viacheslav <viacheslav.rybak.pb@gmail.com>
2023-02-07 07:46:21 +0200

    module 1 task 4 hashtags validation

--- js/form.js
+++ js/form.js
@@ -3,8 +3,6 @@ const uploadPopupClose = document.querySelector('#upload-cancel');
 const body = document.querySelector('body');
 const tagInput = document.querySelector('.text__hashtags');
 
-// photoInput.onChange = onPhotoUpload();
-
 export function onPhotoUpload(){
   console.log('input changed');
   console.log(tagInput.value);
@@ -34,17 +32,84 @@ function closeUploadPopup() {
 function checkTag(){
   const value = this.value
   const space = ' ';
-  const tags = value.split(space);
+  const tagsRaw = value.split(space);
+  const tagsNoSpaces = tagsRaw.filter(value => value != '');
+  const tagsLower = tagsNoSpaces.map(element => {
+    return element.toLowerCase();
+  });
+  const tags = tagsLower;
+
+  const tagMaxLength = 19;
+  const tagsMaxQuantity = 5;
   console.log(tags);
 
+  let validationMessage = '';
+
   tags.forEach(tag => {
-    let chars = tag.split();
+    console.log(tag);
+    const chars = tag.split('');
+
     console.log(chars);
+    // чи починається на #
+    if (chars[0] === '#') {
+      chars.shift();
+    }
+    else {
+      validationMessage = 'Помилка: Теги мають починатись на # і не містити пробілів.';
+      console.log('Помилка: Нема решітки на початку');
+    }
+    // чи містить сторонні символи
+    if (/^[A-Za-z0-9]*$/.test(chars.join(''))) {
+      console.log(chars.join(''));
+    }
+    else {
+      validationMessage = 'Помилка: Теги мають містити тільки букви та цифри.';
+      console.log('Помилка: Сторонні символи');
+    }
+    // чи складається тільки з решітки
+    if (chars.length === 0) {
+      validationMessage = 'Помилка: Тег не може складатися тільки з одних ґрат.';
+      console.log('Помилка: Одні грати');
+    }
+    // чи довжина до 20 символів
+    if (chars.length > tagMaxLength) {
+      validationMessage = 'Помилка: Максимальна довжина тегу - 20 символів.';
+      console.log('Помилка: Більше 20 символів.');
+    }
   });
-  // check every tag
 
+  // чи тегів не більше 5
+  if (tags.length > tagsMaxQuantity) {
+    validationMessage = 'Помилка: Тегів має бути не більше п\'яти';
+    console.log('Помилка: Більше 5 тегів');
+  }
+
+  // перевірити повторки
+  let alreadySeen = {};
+  tags.forEach(function(str) {
+    if (alreadySeen[str]) {
+      console.log(`Тег ${str} повтрюється.`);
+      validationMessage = `Помилка: Тег ${str} повтрюється, треба прибрати повтор`;
+    }
+    else {
+      alreadySeen[str] = true;
+    }
+  });
+
+  // вивести помилку
+  if (validationMessage) {
+    tagInput.setCustomValidity(validationMessage);
+    tagInput.reportValidity();
+  }
+  // вивести корректні теги отримані в підсумку
+  else {
+    const successsMessage = `Чудово! Ваші теги: ${tags.join()}`;
+    tagInput.setCustomValidity(successsMessage);
+    tagInput.reportValidity();
+  }
 }
-
 /*
-#hey  ##hi #!hello #ho%lla #aloha #hehe
- */
+Тестуємо теги:
+#hey  #HEY ##hi # #!HELLO #ho%lla aloha #hehe
+#hey #HEY #HELLO #holla #3
+*/
